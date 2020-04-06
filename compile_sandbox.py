f1aa7f464efd88b62dc9bbe6ed9bcd1ef469c59a
robo-monk <filtap007@gmail.com>
2020-04-06 21:34:49 +0300

    add sandbox compile script

---
+++ compile_sandbox.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+with open('view_files/header.txt', 'r') as file:
+    header = file.read().replace('\n', '')
+    
+with open('view_files/body_upper.txt', 'r') as file:
+    body_upper = file.read().replace('\n', '')
+    
+with open('view_files/api_response.txt') as file:
+    api_res = file.read().replace('\n', '')
+
+with open('view_files/body_downer.txt', 'r') as file:
+    body_downer = file.read().replace('\n', '')
+
+
+compiled_html = "<!DOCTYPE html> <html>" + header + body_upper + api_res + body_downer + "</html>"
+
+
+file = open("index.html", "w")
+file.write(compiled_html)
+file.close()
