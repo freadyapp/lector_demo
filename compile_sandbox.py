f3e063b16e751d0946dfb9ae089b0454762a55cb
robo-monk <filtap007@gmail.com>
2020-04-17 20:03:34 +0300

    update env

--- compile_sandbox.py
+++ compile_sandbox.py
@@ -6,14 +6,14 @@ with open('view_files/header.txt', 'r') as file:
 with open('view_files/body_upper.txt', 'r') as file:
     body_upper = file.read().replace('\n', '')
     
-with open('view_files/api_response.txt') as file:
-    api_res = file.read().replace('\n', '')
+with open('view_files/reader.txt') as file:
+    reader = file.read().replace('\n', '')
 
 with open('view_files/body_downer.txt', 'r') as file:
     body_downer = file.read().replace('\n', '')
 
 
-compiled_html = "<!DOCTYPE html> <html>" + header + body_upper + api_res + body_downer + "</html>"
+compiled_html = "<!DOCTYPE html> <html>" + header + body_upper + reader + body_downer + "</html>"
 
 file = open("index.html", "w")
 file.write(compiled_html)
